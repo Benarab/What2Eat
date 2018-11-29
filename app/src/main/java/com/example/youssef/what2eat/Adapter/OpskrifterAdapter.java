8ac9f78e1645821e40506d64ade895a1164f9ae3
Benarab <Youssef.Benarab@hotmail.com>
2018-11-29 12:11:15 +0100

    j

---
+++ app/src/main/java/com/example/youssef/what2eat/Adapter/OpskrifterAdapter.java
@@ -0,0 +1,66 @@
+package com.example.youssef.what2eat.Adapter;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.youssef.what2eat.Models.Opskrifter;
+import com.example.youssef.what2eat.R;
+
+import java.util.List;
+
+public class OpskrifterAdapter extends ArrayAdapter<Opskrifter> {
+
+    private Context context;
+    private List<Opskrifter> values;
+
+    public OpskrifterAdapter(Context context, List<Opskrifter> values) {
+        super(context, R.layout.custom_opskrifter_list, values);
+
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null) {
+            LayoutInflater inflater =
+                    (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.custom_opskrifter_list, parent, false);
+        }
+
+        ImageView imgOpskrift = (ImageView) row.findViewById(R.id.img_opskrift);
+        TextView tvOpskriftnavn = (TextView) row.findViewById(R.id.tv_opskriftNavn);
+        TextView tvRating = (TextView) row.findViewById(R.id.tv_rating);
+        TextView tvTilberedelsestid = (TextView) row.findViewById(R.id.tv_tilberedelsestid);
+        TextView tvKategori = (TextView) row.findViewById(R.id.tv_kategori);
+        TextView tvGenre = (TextView) row.findViewById(R.id.tv_genre);
+        TextView tvIngredienser = (TextView) row.findViewById(R.id.tv_ingredienser);
+        TextView tvFremgangsmåde = (TextView) row.findViewById(R.id.tv_fremgangsmåde);
+
+
+        Opskrifter item = values.get(position);
+        Image billede = item.getBillede();
+        String navn = item.getNavn();
+        int rating = item.getRating();
+        int varighed = item.getVarighed();
+        String kategori = item.getKategori();
+        String genre = item.getGenre();
+        String beskrivelse = item.getBeskrivelse();
+        tvOpskriftnavn.setText(navn);
+        tvRating.setText(rating);
+        tvTilberedelsestid.setText(varighed);
+        tvKategori.setText(kategori);
+        tvGenre.setText(genre);
+        tvFremgangsmåde.setText(beskrivelse);
+
+        return row;
+    }
+}
