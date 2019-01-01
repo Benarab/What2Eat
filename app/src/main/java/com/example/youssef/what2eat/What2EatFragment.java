cc7cc1168f2d029ed6af074bda71b055b19421ce
Benarab <Youssef.Benarab@hotmail.com>
2019-01-01 16:57:24 +0100

    Der skal forbindes til custom_ingredienser_list

--- app/src/main/java/com/example/youssef/what2eat/What2EatFragment.java
+++ app/src/main/java/com/example/youssef/what2eat/What2EatFragment.java
@@ -6,17 +6,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.youssef.what2eat.Adapter.OpskrifterAdapter;
 import com.example.youssef.what2eat.Models.Ingredienser;
@@ -27,41 +32,60 @@ import java.util.ArrayList;
 
 public class What2EatFragment extends Fragment {
 
-    ImageButton imgButton;
-    EditText et_igredienser;
-    GridView gridview;
+    private static final String[] INGREDIENSER = new String[]{
+            "Salt", "Pepper", "Mælk", "Æg", "Mel", "Sukker", "Vand", "Smør", "Olie", "Flormelis", "Gær", "Chokolade", "Kartofler", "Tomater", "Argurk", "Ost", "Kylling", "Hakket oksekød", "Bagepulver"
+    };
+
+    ImageButton btn_add_ingredient;
+    AutoCompleteTextView et_igredienser;
+    ListView lv_what2eat;
     ListView søgeResultaterList;
     ArrayAdapter<String> adapter;
     OpskrifterAdapter adapterOpskrift, adapterSøgeResultater;
-    Button search_button;
+    FloatingActionButton search_button;
     ArrayList<Opskrifter> resultat_opskrifter;
+    TextView tv_ingrediens;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_what2eat, container, false);
+        final View view = inflater.inflate(R.layout.fragment_what2eat, container, false);
+
         søgeResultaterList = (ListView) view.findViewById((R.id.list_søgeresultater));
-        search_button = (Button) view.findViewById((R.id.søg_knap));
-        imgButton = (ImageButton) view.findViewById(R.id.search_button);
-        et_igredienser = (EditText) view.findViewById(R.id.id_search_EditText);
-        gridview = (GridView) view.findViewById(R.id.grid_viewList);
+        search_button = (FloatingActionButton) view.findViewById((R.id.søg_knap));
+        btn_add_ingredient = (ImageButton) view.findViewById(R.id.search_button);
+        et_igredienser = (AutoCompleteTextView) view.findViewById(R.id.id_search_EditText);
+        lv_what2eat = (ListView) view.findViewById(R.id.lv_what2eat);
+        tv_ingrediens = (TextView) view.findViewById(R.id.tv_ingrediens);
         resultat_opskrifter = new ArrayList<>();
         adapterOpskrift = new OpskrifterAdapter(getContext(), MainActivity.lokale_opskrifters);
         adapterSøgeResultater = new OpskrifterAdapter(getContext(), resultat_opskrifter);
         søgeResultaterList.setAdapter(adapterOpskrift);
 
-        adapter = new ArrayAdapter<String>(getContext(), R.layout.cell, MainActivity.søge_ingredienser);
-        gridview.setAdapter(adapter);
-        imgButton.setOnClickListener(new View.OnClickListener() {
+        et_igredienser.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    et_igredienser.setHint("");
+                InputMethodManager imm = (InputMethodManager)   getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+            }
+        });
+
+        adapter = new ArrayAdapter<String>(getContext(), R.layout.custom_ingredienser_list, MainActivity.søge_ingredienser);
+        lv_what2eat.setAdapter(adapter);
+        btn_add_ingredient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MainActivity.søge_ingredienser.add(et_igredienser.getText().toString().trim());
-                et_igredienser.setText(null);
+                if (et_igredienser.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Du har ikke angivet nogen ingrediens", Toast.LENGTH_SHORT).show();
+                } else {
+                    MainActivity.søge_ingredienser.add(et_igredienser.getText().toString().trim());
+                    et_igredienser.setText(null);
 
-                adapter.notifyDataSetChanged();
-                gridview.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    lv_what2eat.setAdapter(adapter);
+                }
             }
-
         });
 
         search_button.setOnClickListener(new View.OnClickListener() {
@@ -82,13 +106,16 @@ public class What2EatFragment extends Fragment {
 
 
                 Log.d("Resultat: ", "" + resultat_opskrifter);
-                search_button.setVisibility(view.GONE);
+                //search_button.setVisibility(view.GONE);
                 adapterOpskrift.notifyDataSetChanged();
                 søgeResultaterList.setAdapter(adapterSøgeResultater);
                 søgeResultaterList.setVisibility(View.VISIBLE);
             }
         });
 
+        ArrayAdapter<String> adapter_ingredienser = new ArrayAdapter<String>(getContext(),
+                R.layout.custom_autotextview, R.id.text_view_list_item, INGREDIENSER);
+        et_igredienser.setAdapter(adapter_ingredienser);
 
         return view;
     }
